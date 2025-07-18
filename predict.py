cde07919e7ad8d23be33e23441810b3cb5f8fb8d
Barashov <barashovmisha@gmail.com>
2025-07-18 19:34:12 +0500

    feat: нагрузка 2.0

--- predict.py
+++ predict.py
@@ -1,22 +1,15 @@
-from cog import BasePredictor, Input, Path, BaseModel
-import whisperx
-import requests
-import gc
-from datetime import datetime
-import os
-import time
+from cog import BasePredictor, BaseModel, Input, Path
+from faster_whisper import WhisperModel
 
 
 class Predictor(BasePredictor):
     def setup(self):
-        device = "cuda"
-        batch_size = 16  # reduce if low on GPU mem
-        compute_type = "float16"
-        start_time_whisper = datetime.now()
-        self.whisper_model = whisperx.load_model("large-v2", device, compute_type=compute_type)
-        print(f'Время загрузки модели транскрибации: {datetime.now() - start_time_whisper}')
-
-        start_time_diarize = datetime.now()
+        model_name = "large-v3-turbo"
+        self.model = WhisperModel(
+            model_name,
+            device="cuda",
+            compute_type="float16",
+        )
 
     def predict(self, text: str) -> str:
         return f"Ты написал: {text}"
